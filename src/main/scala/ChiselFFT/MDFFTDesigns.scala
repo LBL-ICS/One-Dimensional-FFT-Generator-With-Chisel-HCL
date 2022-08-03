2995da4f05e97e899860a8114819b089c547c66b
vegamario <vegam5954@uhcl.edu>
2022-08-03 14:40:01 -0700

    TEST

--- src/main/scala/ChiselFFT/MDFFTDesigns.scala
+++ src/main/scala/ChiselFFT/MDFFTDesigns.scala
@@ -28,7 +28,7 @@ object MDFFTDesigns {
     out_regdelay := regdelays(TotalLatency-1)
     io.out_validate := out_regdelay
     io.out := out_regs
-//    io.out_test := Permutes.out
+    //    io.out_test := Permutes.out
     for(i <- 1 until TotalLatency){
       regdelays(i) := regdelays(i-1)
     }
@@ -57,5 +57,98 @@ object MDFFTDesigns {
         }
       }
     }
+  }
+
+  def getfftstreamingmrlatency(N:Int, nr: Int, ns: Int, r: Int, s: Int, w: Int, bw: Int): Int = { // streaming single radix fft, still in progress
+    val w1 = w // this will be the input and output width
+    var w2 = Permutations.getw2(w1, N, nr, ns, s)
+    var delay_cycles_stage2 = N / w1 - N / w2 // w2 will be bigger than w1
+    // for the first N/w2 clock cycles, the output from the permutation should have a corresponding valid signal,
+    // however, for the remaint N/w1-N/w2 clock cycles, the output from the permutation should have an invalid signal
+    val CMultLatency = 2
+    val T_L = CMultLatency
+    var fftlatency1 = 0
+    var fftlatency2 = 0
+    val perm_latency1 = (N / w1) * 2
+    if(w1 < nr && w2 < ns){
+      fftlatency1 = getfftstreamedlatency(nr,r,w1,bw)
+      fftlatency2 = getfftstreamedlatency(ns,s,w2,bw)
+    }else if(w1 < nr && w2 >= ns){
+      fftlatency1 = getfftstreamedlatency(nr,r,w1,bw)
+      fftlatency2 = getFFTLatency(ns,s,ns,bw)
+    }else if(w1 >= nr && w2 < ns){
+      fftlatency1 = getFFTLatency(nr,r,nr,bw)
+      fftlatency2 = getfftstreamedlatency(ns,s,w2, bw)
+    }else if(w1 >= nr && w2 >= ns){
+      fftlatency1 = getFFTLatency(nr,r,nr,bw)
+      fftlatency2 = getFFTLatency(ns,s,ns,bw)
+    }
+    val total_latency = perm_latency1*3 + T_L + fftlatency1 + fftlatency2 + 1 // add 1 for output register
+    total_latency
+  }
+
+  class FFT3D(N: Int, win: Int, nr: Int, ns: Int, r: Int, s: Int, bw:Int) extends Module{ // simple streaming version
+    val io = IO(new Bundle() {
+      val in = Input(Vec(win, new ComplexNum(bw)))
+      val in_ready = Input(Bool())
+      val out_validate = Output(Bool())
+      val out = Output(Vec(win, new ComplexNum(bw)))
+    })
+    val FFT1Ds = VecInit.fill(3)(Module(new FFT_mr_v2_streamingv2(N,nr,ns,r,s,win,bw)).io) // we need three of em for streaming
+    val Permutes = VecInit.fill(3)(Module(new PermutationsWithStreaming(N*N*N,N,N,win,0,bw)).io) // one permutation module
+
+    val FFT1DLatency = getfftstreamingmrlatency(N,nr,ns,r,s,win,bw)
+    val Permutes_latency = (N*N*N/win)*2
+    val TotalLatency = FFT1DLatency*3 + Permutes_latency*3 + 1
+
+    val perm_regdelays = RegInit(VecInit.fill(3)(VecInit.fill(Permutes_latency)(false.B)))
+    val FFT1D_regdelays = RegInit(VecInit.fill(3)(VecInit.fill(FFT1DLatency)(false.B)))
+
+    val out_regdelay = RegInit(false.B)
+    val out_regs = RegInit(VecInit.fill(win)(0.U.asTypeOf(new ComplexNum(bw))))
+    FFT1D_regdelays(0)(0) := io.in_ready
+    FFT1D_regdelays(1)(0) := perm_regdelays(0)(Permutes_latency-1)
+    FFT1D_regdelays(2)(0) := perm_regdelays(1)(Permutes_latency-1)
+    out_regdelay :=          perm_regdelays(2)(Permutes_latency-1)
+    perm_regdelays(0)(0) := FFT1D_regdelays(0)(FFT1DLatency-1)
+    perm_regdelays(1)(0) := FFT1D_regdelays(1)(FFT1DLatency-1)
+    perm_regdelays(2)(0) := FFT1D_regdelays(2)(FFT1DLatency-1)
+
+    Permutes(0).in_en(Permutes_latency) := perm_regdelays(0)(Permutes_latency-1)
+    Permutes(1).in_en(Permutes_latency) := perm_regdelays(1)(Permutes_latency-1)
+    Permutes(2).in_en(Permutes_latency) := perm_regdelays(2)(Permutes_latency-1)
+    Permutes(0).in_en(0) := FFT1D_regdelays(0)(FFT1DLatency-1)
+    Permutes(1).in_en(0) := FFT1D_regdelays(1)(FFT1DLatency-1)
+    Permutes(2).in_en(0) := FFT1D_regdelays(2)(FFT1DLatency-1)
+    FFT1Ds(0).in_ready := io.in_ready
+    FFT1Ds(1).in_ready := perm_regdelays(0)(Permutes_latency-1)
+    FFT1Ds(2).in_ready := perm_regdelays(1)(Permutes_latency-1)
+    when(io.in_ready){
+      FFT1Ds(0).in := io.in
+    }.otherwise{
+      FFT1Ds(0).in := VecInit.fill(win)(0.U.asTypeOf(new ComplexNum(bw)))
+    }
+
+    FFT1Ds(1).in := Permutes(0).out
+    FFT1Ds(2).in := Permutes(1).out
+    Permutes(0).in := FFT1Ds(0).out
+    Permutes(1).in := FFT1Ds(1).out
+    Permutes(2).in := FFT1Ds(2).out
+    for(i <- 0 until 3){
+      for(j <- 1 until FFT1DLatency){
+        FFT1D_regdelays(i)(j) := FFT1D_regdelays(i)(j-1)
+      }
+      for(j <- 1 until Permutes_latency){
+        perm_regdelays(i)(j) := perm_regdelays(i)(j-1)
+        Permutes(i).in_en(j) := perm_regdelays(i)(j-1)
+      }
+    }
+    when(perm_regdelays(2)(Permutes_latency-1)){
+      out_regs := Permutes(2).out
+    }.otherwise{
+      out_regs := VecInit.fill(win)(0.U.asTypeOf(new ComplexNum(bw)))
+    }
+    io.out_validate := out_regdelay
+    io.out := out_regs
   }
 }
--- src/main/scala/GoldenModels/FFT_GoldenModels.scala
+++ src/main/scala/GoldenModels/FFT_GoldenModels.scala
@@ -18,13 +18,13 @@ import better.files._
 import scala.util.Random
 
 object FFT_GoldenModels {
-    def FFT_r_GoldenModel(N: Int, r: Int, inputs: Array[Array[cmplx]]): Array[Array[cmplx]] = { // 2D array holding FFT results for each input group
-      val solns = (for(i <- 0 until inputs.length)yield{
-        val soln = FFT_r(N,r,inputs(i))
-        soln
-      }).toArray
-      solns
-    }
+  def FFT_r_GoldenModel(N: Int, r: Int, inputs: Array[Array[cmplx]]): Array[Array[cmplx]] = { // 2D array holding FFT results for each input group
+    val solns = (for(i <- 0 until inputs.length)yield{
+      val soln = FFT_r(N,r,inputs(i))
+      soln
+    }).toArray
+    solns
+  }
 
   def FFTMD_GoldenModel(d: Int, N: Int, inputs: Array[Array[cmplx]], FFT_Out_Files:IndexedSeq[(PrintWriter,PrintWriter)], Trans_Out_Files:IndexedSeq[(PrintWriter,PrintWriter)]): Array[Array[cmplx]] = { // 2D array holding FFT results for each input group
     val solns = (for(i <- 0 until inputs.length)yield{
@@ -34,27 +34,27 @@ object FFT_GoldenModels {
     solns
   }
 
-    def genRandom(n: Int, name: String,bw:Int): Unit = {
-      val newfile2 = new File("./InputOutputFiles/" +  name+ ".txt")
-      val newfile1 = new File("./InputOutputFiles/" +  name+"_Decimal.txt")
-      val pw = new PrintWriter(newfile1)
-      val pw2 = new PrintWriter(newfile2)
-      val start = -5000000000.0.toLong
-      val end = 5000000000.0.toLong
-      val rnd = new Random()
-      for (i <- 0 until n) {
-        val temp = (start + rnd.nextLong((end - start) + 1)) * 0.000000001 + 0.01
-        if (i == n - 1) {
-          pw.print(temp)
-          pw2.print(convert_string_to_IEEE_754(temp.toString, bw).toLong.toHexString)
-        } else {
-          pw.println(temp)
-          pw2.println(convert_string_to_IEEE_754(temp.toString, bw).toLong.toHexString)
-        }
+  def genRandom(n: Int, name: String,bw:Int): Unit = {
+    val newfile2 = new File("./InputOutputFiles/" +  name+ ".txt")
+    val newfile1 = new File("./InputOutputFiles/" +  name+"_Decimal.txt")
+    val pw = new PrintWriter(newfile1)
+    val pw2 = new PrintWriter(newfile2)
+    val start = -5000000000.0.toLong
+    val end = 5000000000.0.toLong
+    val rnd = new Random()
+    for (i <- 0 until n) {
+      val temp = (start + rnd.nextLong((end - start) + 1)) * 0.000000001 + 0.1
+      if (i == n - 1) {
+        pw.print(temp)
+        pw2.print(convert_string_to_IEEE_754(temp.toString, bw).toLong.toHexString)
+      } else {
+        pw.println(temp)
+        pw2.println(convert_string_to_IEEE_754(temp.toString, bw).toLong.toHexString)
       }
-      pw.close()
-      pw2.close()
     }
+    pw.close()
+    pw2.close()
+  }
   def genRandom2(n: Int, name: String,bw:Int): Unit = {
     val newfile2 = new File("./InputOutputFiles_SingleInput/" +  name+ ".txt")
     val newfile1 = new File("./InputOutputFiles_SingleInput/" +  name+"_Decimal.txt")
@@ -64,7 +64,7 @@ object FFT_GoldenModels {
     val end = 5000000000.0.toLong
     val rnd = new Random()
     for (i <- 0 until n) {
-      val temp = (start + rnd.nextLong((end - start) + 1)) * 0.000000001 +0.01
+      val temp = (start + rnd.nextLong((end - start) + 1)) * 0.000000001 +0.1
       if (i == n - 1) {
         pw.print(temp)
         pw2.print(convert_string_to_IEEE_754(temp.toString, bw).toLong.toHexString)
@@ -85,7 +85,7 @@ object FFT_GoldenModels {
     val end = 5000000000.0.toLong
     val rnd = new Random()
     for (i <- 0 until n) {
-      val temp = (start + rnd.nextLong((end - start) + 1)) * 0.000000001 +0.01
+      val temp = (start + rnd.nextLong((end - start) + 1)) * 0.00000001 +0.1
       if (i == n - 1) {
         pw.print(temp)
         pw2.print(convert_string_to_IEEE_754(temp.toString, bw).toLong.toHexString)
@@ -140,15 +140,15 @@ object FFT_GoldenModels {
     val subdirectory = file"./InputOutputFiles_SingleInput"
     subdirectory.clear()
     genRandom2(N*2*runs,s"inputfile",bw)
-//    for(i <- 0 until N){
-//      genRandom(2*runs,s"inputfile${i}",bw)
-//    }
+    //    for(i <- 0 until N){
+    //      genRandom(2*runs,s"inputfile${i}",bw)
+    //    }
     var file = scala.io.Source.fromFile(s"./InputOutputFiles_SingleInput/inputfile_Decimal.txt").getLines().map(x=>x.toDouble).toArray
     //file.toArray
-//    val getFiles = (for(i <- 0 until N) yield{
-//      val file = scala.io.Source.fromFile(s"./InputOutputFiles_SingleInput/inputfile${i}_Decimal.txt").getLines().map(x=>x.toDouble)
-//      file.toArray
-//    }).toArray
+    //    val getFiles = (for(i <- 0 until N) yield{
+    //      val file = scala.io.Source.fromFile(s"./InputOutputFiles_SingleInput/inputfile${i}_Decimal.txt").getLines().map(x=>x.toDouble)
+    //      file.toArray
+    //    }).toArray
     val cmplxFiles = (for(i <- 0 until runs) yield{
       (for(j <- 0 until 2*N by 2) yield{
         cmplx(file(i*2*N + j),file(i*2*N + j+1))
@@ -238,82 +238,82 @@ object FFT_GoldenModels {
     val runs = 1 // number of runs for inputs/outputs to be generated
     //val name = "TestInputs"
     //genRandom(N*2*runs,name, bw) // generate about twice of the total N for all runs, because we are working with complex numbes
-//    genDFTInOutFile(N,r,bw,runs) // for example
-//    genDFTInOutFile_single_input_file(N,r,bw,runs) // for single input file
+    //    genDFTInOutFile(N,r,bw,runs) // for example
+    //    genDFTInOutFile_single_input_file(N,r,bw,runs) // for single input file
     val d = 3
     genDFTInOutFile_MD(d,N,bw, runs)
-//    val DFTs_per_stage = N/r
-//    val number_of_stages = (Math.log10(N)/Math.log10(r)).round.toInt
-//    val CMultLatency = 2
-//    val CAddLatency = 1
-//    val DFT_latency = CMultLatency + ((Math.log10(r)/Math.log10(2)).round.toInt + (for(l <- 0 until (Math.log10(r)/Math.log10(2)).round.toInt)yield{(r/Math.pow(2,l)).round.toInt % 2}).reduce(_+_)) * (CAddLatency)
-//    val Twid_latency = (N/w) * CMultLatency
-//    val Perm_latency = 0
-//    val Total_Latency = (number_of_stages - 1) * Twid_latency + (number_of_stages) * DFT_latency + (number_of_stages + 1) * Perm_latency
+    //    val DFTs_per_stage = N/r
+    //    val number_of_stages = (Math.log10(N)/Math.log10(r)).round.toInt
+    //    val CMultLatency = 2
+    //    val CAddLatency = 1
+    //    val DFT_latency = CMultLatency + ((Math.log10(r)/Math.log10(2)).round.toInt + (for(l <- 0 until (Math.log10(r)/Math.log10(2)).round.toInt)yield{(r/Math.pow(2,l)).round.toInt % 2}).reduce(_+_)) * (CAddLatency)
+    //    val Twid_latency = (N/w) * CMultLatency
+    //    val Perm_latency = 0
+    //    val Total_Latency = (number_of_stages - 1) * Twid_latency + (number_of_stages) * DFT_latency + (number_of_stages + 1) * Perm_latency
 
-//    val golden_input = scala.io.Source.fromFile(name + "_Decimal.txt").getLines().map(x=>x.toDouble).toArray
-//    val pw2 = new PrintWriter("inputfile0.txt")
-//    val pw3 = new PrintWriter("inputfile1.txt")
-//    pw2.close()
-//    pw3.close()
-//    var index = 0
-//    val cmplx_inputs = (for(j <- 0 until runs)yield{
-//      val inp = (for(i <- 0 until N) yield{
-//        val temp = cmplx(golden_input(index),golden_input(index+1))
-//        index+=2
-//        temp
-//      }).toArray
-//      inp
-//    }).toArray
-//    cmplx_inputs.map(x=>x.map(y=>println(y.re)))
-//    val sw_model = FFT_r_GoldenModel(N, r, cmplx_inputs)
-//
-//    val pw1 = new PrintWriter("outputfile.txt")
-//    for(i <- 0 until runs){
-//      for(j<- 0 until N){
-//        pw1.println(convert_string_to_IEEE_754(sw_model(i)(j).re.toString, bw).toLong.toHexString)
-//        pw1.println(convert_string_to_IEEE_754(sw_model(i)(j).im.toString, bw).toLong.toHexString)
-//      }
-//    }
-//    pw1.close()
+    //    val golden_input = scala.io.Source.fromFile(name + "_Decimal.txt").getLines().map(x=>x.toDouble).toArray
+    //    val pw2 = new PrintWriter("inputfile0.txt")
+    //    val pw3 = new PrintWriter("inputfile1.txt")
+    //    pw2.close()
+    //    pw3.close()
+    //    var index = 0
+    //    val cmplx_inputs = (for(j <- 0 until runs)yield{
+    //      val inp = (for(i <- 0 until N) yield{
+    //        val temp = cmplx(golden_input(index),golden_input(index+1))
+    //        index+=2
+    //        temp
+    //      }).toArray
+    //      inp
+    //    }).toArray
+    //    cmplx_inputs.map(x=>x.map(y=>println(y.re)))
+    //    val sw_model = FFT_r_GoldenModel(N, r, cmplx_inputs)
+    //
+    //    val pw1 = new PrintWriter("outputfile.txt")
+    //    for(i <- 0 until runs){
+    //      for(j<- 0 until N){
+    //        pw1.println(convert_string_to_IEEE_754(sw_model(i)(j).re.toString, bw).toLong.toHexString)
+    //        pw1.println(convert_string_to_IEEE_754(sw_model(i)(j).im.toString, bw).toLong.toHexString)
+    //      }
+    //    }
+    //    pw1.close()
 
-//    test(new FFT_sr(N,r,w,bw)){c=>
-//      val input_List = scala.io.Source.fromFile(name + ".txt").getLines().map(x=>convert_string_to_IEEE_754(x, bw)).toArray
-//      val inp3 = input_List.slice(0, input_List.length/2)
-//      val inp4 = input_List.slice(input_List.length/2,input_List.length)
-//      var hw_model = (for(i <- 0 until runs)yield{
-//        val t = (for(j <- 0 until N)yield{
-//          cmplx(0,0)
-//        }).toArray
-//        t
-//      }).toArray
-//      var index = 0
-//      for(j <- 0 until runs) {
-//        for (i <- 0 until N) {
-//          c.io.in(i).Re.poke(input_List(index).U)
-//          println(input_List(index))
-//          c.io.in(i).Im.poke(input_List(index+ 1).U)
-//          index += 2
-//        }
-//        c.clock.step(1)
-//      }
-//      for(j <-0 until runs) {
-//        if(j == 0){
-//          c.clock.step(Total_Latency-runs)
-//        }else{
-//          c.clock.step(1)
-//        }
-//        for (i <- 0 until N) {
-//          val val1 = convert_long_to_float(c.io.out(i).Re.peek().litValue, bw).toDouble
-//          val val2 = convert_long_to_float(c.io.out(i).Im.peek().litValue, bw).toDouble
-//          println(s"Real Output: ${val1}")
-//          println(s"Imaginary Output: ${val2}")
-//          hw_model(j)(i) = cmplx(val1, val2)
-//        }
-//      }
-//      sw_model.map(x=>x.map(y=>y.print_mag)) // sw results stored here
-//      println()
-//      hw_model.map(x=>x.map(y=>y.print_mag)) // hw results stored here
-//    }
+    //    test(new FFT_sr(N,r,w,bw)){c=>
+    //      val input_List = scala.io.Source.fromFile(name + ".txt").getLines().map(x=>convert_string_to_IEEE_754(x, bw)).toArray
+    //      val inp3 = input_List.slice(0, input_List.length/2)
+    //      val inp4 = input_List.slice(input_List.length/2,input_List.length)
+    //      var hw_model = (for(i <- 0 until runs)yield{
+    //        val t = (for(j <- 0 until N)yield{
+    //          cmplx(0,0)
+    //        }).toArray
+    //        t
+    //      }).toArray
+    //      var index = 0
+    //      for(j <- 0 until runs) {
+    //        for (i <- 0 until N) {
+    //          c.io.in(i).Re.poke(input_List(index).U)
+    //          println(input_List(index))
+    //          c.io.in(i).Im.poke(input_List(index+ 1).U)
+    //          index += 2
+    //        }
+    //        c.clock.step(1)
+    //      }
+    //      for(j <-0 until runs) {
+    //        if(j == 0){
+    //          c.clock.step(Total_Latency-runs)
+    //        }else{
+    //          c.clock.step(1)
+    //        }
+    //        for (i <- 0 until N) {
+    //          val val1 = convert_long_to_float(c.io.out(i).Re.peek().litValue, bw).toDouble
+    //          val val2 = convert_long_to_float(c.io.out(i).Im.peek().litValue, bw).toDouble
+    //          println(s"Real Output: ${val1}")
+    //          println(s"Imaginary Output: ${val2}")
+    //          hw_model(j)(i) = cmplx(val1, val2)
+    //        }
+    //      }
+    //      sw_model.map(x=>x.map(y=>y.print_mag)) // sw results stored here
+    //      println()
+    //      hw_model.map(x=>x.map(y=>y.print_mag)) // hw results stored here
+    //    }
   }
 }
